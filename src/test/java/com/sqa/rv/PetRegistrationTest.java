1bc7ef859ee41bd7431cc3ea39329c16a91e16f8
Ruthel <ruthel_rodriguez@yahoo.com>
2016-05-25 20:54:06 -0700

    Create PetRegistration Test Class

---
+++ src/test/java/com/sqa/rv/PetRegistrationTest.java
@@ -0,0 +1,37 @@
+/**
+ *   File Name: PetRegistrationTest.java<br>
+ *
+ *   Villaespin, Ruthel<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: May 25, 2016
+ *   
+ */
+
+package com.sqa.rv;
+
+import org.junit.*;
+
+/**
+ * PetRegistrationTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Villaespin, Ruthel
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class PetRegistrationTest {
+
+	/**
+	 * Test method for {@link com.sqa.rv.PetRegistration#registerPet()}.
+	 */
+	@Test
+	public void testRegisterPet() {
+		PetRegistration.registerPet();
+	}
+
+}
